548551dec110d6037e747e0c181d72e8166d0f53
Robin Bader <robin.bader@forcklabs.com>
2016-08-15 17:12:49 +0200

    SimulationMap with grid neighbours calculation

---
+++ src/main/scala/CellState.scala
@@ -0,0 +1,4 @@
+object CellState extends Enumeration {
+  val None, Active, Start, End, Wall, Visited, Path
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,17 +4,17 @@ class Main extends PApplet {
   var simulationMap: SimulationMap = _
   var gridVisualizer: GridVisualizer = _
 
-  override def setup: Unit = {
+  override def setup(): Unit = {
     simulationMap = MapBuilder.createMap(System.getProperty("user.dir") + "/maps/map.txt")
     println(simulationMap)
     gridVisualizer = new GridVisualizer(this)
   }
   
-  override def settings: Unit = {
+  override def settings(): Unit = {
     size(800, 800)
   }
 
-  override def draw: Unit = {
+  override def draw(): Unit = {
     gridVisualizer.draw(simulationMap)
   }
 }
